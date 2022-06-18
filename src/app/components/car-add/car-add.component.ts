6d3adb2404452e1fd24c66f11e727e0470af2f53
Ahmet GÜÇKOL <ahmtgckl@gmail.com>
2022-06-18 16:10:55 +0300

    commit

--- src/app/components/car-add/car-add.component.ts
+++ src/app/components/car-add/car-add.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { Brand } from 'src/app/models/brand';
+import { Color } from 'src/app/models/color';
+import { BrandService } from 'src/app/services/brand.service';
 import { CarService } from 'src/app/services/car.service';
+import { ColorService } from 'src/app/services/color.service';
 
 @Component({
   selector: 'app-car-add',
@@ -10,15 +15,24 @@ import { CarService } from 'src/app/services/car.service';
 })
 export class CarAddComponent implements OnInit {
   carAddForm: FormGroup;
+  brands:Brand[]=[]
+  colors:Color[]=[]
+  selectedBrand=0
+  selectedColor=0
 
   constructor(
     private carService: CarService,
+    private brandService:BrandService,
+    private colorService:ColorService,
     private formBuilder: FormBuilder,
-    private toastrService: ToastrService
+    private toastrService: ToastrService,
+    private route:Router
   ) { }
 
   ngOnInit(): void {
-    this.createCarAddForm();
+    this.createCarAddForm()
+    this.getBrands()
+    this.getColors()
   }
 
   createCarAddForm() {
@@ -31,6 +45,9 @@ export class CarAddComponent implements OnInit {
       description: ['', Validators.required],
     });
   }
+
+
+
 
   add() {
     if (this.carAddForm.valid) {
@@ -50,4 +67,21 @@ export class CarAddComponent implements OnInit {
       this.toastrService.error('Formunuz eksik', 'Dikkat');
     }
   }
+
+
+
+  getBrands(){
+    this.brandService.getBrands().subscribe(response=>{
+      this.brands=response.data
+    })
+  }
+  getColors(){
+    this.colorService.getColors().subscribe(response=>{
+      this.colors=response.data
+    })
+  }
+
+
+
+
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { LoginModel } from '../models/loginModel';
+import { SingleResponseModel } from '../models/singleResponseModel';
+import { TokenModel } from '../models/tokenModel';
+import { LocalstorageService } from './localstorage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  apiUrl = 'https://localhost:44310/api/auth/';
+
+  constructor(private httpClient: HttpClient,
+    private localStorageService: LocalstorageService) { }
+
+
+  login(loginModel: LoginModel) {
+    return this.httpClient.post<SingleResponseModel<TokenModel>>(this.apiUrl + "login", loginModel)
+  }
+
+
+
+
+  isAuthenticated() {
+    if (localStorage.getItem("token")) {
+      return true;
+
+    } else {
+      return false;
+    }
+  }
+
+
+
+
+
+
+
 }
